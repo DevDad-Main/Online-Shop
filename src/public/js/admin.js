3be841f99a48a266b6789c889478f61144f0d108
Oliver <softwaredevdad@gmail.com>
2025-07-19 16:05:11 +0200

    deleting the whole article for the product card

--- src/public/js/admin.js
+++ src/public/js/admin.js
@@ -2,6 +2,9 @@ const deleteProduct = (btn) => {
   const prodId = btn.parentNode.querySelector("[name=productId]").value;
   const csrf = btn.parentNode.querySelector("[name=_csrf]").value;
 
+  // Getting the root article of this product card in which we want to delete
+  const productElement = btn.closest("article");
+
   // Fetch is not just used for fetching data, we can actually send data with it aswell
   fetch(`/admin/product/${prodId}`, {
     method: "DELETE",
@@ -10,7 +13,14 @@ const deleteProduct = (btn) => {
     },
   })
     .then((result) => {
-      console.log(result);
+      return result.json();
+    })
+    .then((data) => {
+      console.log(data);
+      // This does not work in internet explorer but in every other browser
+      // productElement.remove();
+      // This will work in every browser
+      productElement.parentNode.removeChild(productElement);
     })
     .catch((err) => {
       console.log(err);
